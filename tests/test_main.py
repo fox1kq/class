33cecc9191b9fd9ed269585558f860a1fcef25b2
Padusev.D <735@gmail.com>
2025-05-13 14:05:40 +0300

    14.2

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,46 +1,23 @@
 import pytest
+from src.main import Product
 
+def test_price_setter_validation(monkeypatch):
+    p = Product("Хлеб", "Белый хлеб", 40, 10)
 
+    # Попытка установить цену < 0
+    p.price = -10
+    assert p.price == 40
 
-@pytest.fixture
-def products():
-    return [
-        Product("iPhone 15", "512GB, Gray", 210000.0, 8),
-        Product("Galaxy S23 Ultra", "256GB, Серый цвет", 180000.0, 5),
-        Product("Redmi Note 11", "1024GB, Синий", 31000.0, 14),
-    ]
+    # Попытка установить цену = 0
+    p.price = 0
+    assert p.price == 40
 
-@pytest.fixture
-def category(products):
-    # Сброс счётчиков перед тестом
-    Category.category_count = 0
-    Category.product_count = 0
+    # Попытка понизить цену, пользователь отвечает "n"
+    monkeypatch.setattr("builtins.input", lambda _: "n")
+    p.price = 30
+    assert p.price == 40
 
-    return Category("Смартфоны", "Умные телефоны нового поколения", products)
-
-def test_product_init(products):
-    product = products[0]
-    assert product.name == "iPhone 15"
-    assert product.description == "512GB, Gray"
-    assert product.price == 210000.0
-    assert product.quantity == 8
-
-def test_category_init(category):
-    assert category.name == "Смартфоны"
-    assert category.description == "Умные телефоны нового поколения"
-    assert len(category.products) == 3
-
-def test_category_counts(category):
-    assert Category.category_count == 1
-    assert Category.product_count == 3
-
-def test_category_with_more_products():
-    Category.category_count = 0
-    Category.product_count = 0
-
-    p1 = Product("TV", "QLED 55\"", 123000.0, 7)
-    p2 = Product("Monitor", "24\" FullHD", 23000.0, 12)
-    c = Category("Техника", "Для дома", [p1, p2])
-
-    assert Category.category_count == 1
-    assert Category.product_count == 2
+    # Попытка понизить цену, пользователь отвечает "y"
+    monkeypatch.setattr("builtins.input", lambda _: "y")
+    p.price = 30
+    assert p.price == 30
